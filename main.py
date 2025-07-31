06bad92dfb339b3bbfcc51dc39b6e384a05f1677
unknown <PAOLAAGUIRRE>
2025-07-30 21:46:33 -0500

    Se importo la libreria nltk al main

---
+++ main.py
@@ -0,0 +1,7 @@
+# importar la libreria nltk
+import nltk
+# desde nltk descargar el paquete stopwords
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+nltk.data.path.append('C:\Users\Paola\AppData\Roaming\nltk_data')
--- stopwords.py
+++ stopwords.py
@@ -6,3 +6,5 @@ nltk.download('stopwords')
 lista_stopwords = stopwords.words('spanish')
 #imprimir las stopwords
 print(lista_stopwords)
+nltk.data.path.append('C:\Users\Paola\AppData\Roaming\nltk_data')
+
